f2c3253ba687d1178e197df89d634dcbadaf741f
Ole Gunhildsberg Hansen <ogh@computas.com>
2018-11-03 14:11:45 +0100

    Added Test result class

--- SpeedTestApi/Controllers/SpeedTestController.cs
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpeedTestApi.Models;
 
 namespace SpeedTestApi.Controllers
 {
@@ -15,6 +16,14 @@ namespace SpeedTestApi.Controllers
         public ActionResult<string> Ping()
         {
             return Ok("PONG");
+        }
+
+        [HttpPost]
+        public ActionResult<string> UploadSpeedTest([FromBody] TestResult speedTest)
+        {
+            var speedTestData = $"Got a TestResult from { speedTest.User } with download { speedTest.Data.Speeds.Download } Mbps.";
+
+            return Ok(speedTestData);
         }
     }
 }
